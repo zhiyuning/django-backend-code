d783fe021c62d98905e3ed4d5fe759b206f1dee9
ZHIYUNING <zn2108@nyu.edu>
2024-07-14 22:19:53 -0400

    upload code

---
+++ tasks/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from rest_framework import generics
+from .models import Task
+from .serializers import TaskSerializer
+
+class TaskListCreate(generics.ListCreateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+class TaskRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
